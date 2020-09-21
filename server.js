edab646124a65a436c2319db981f15f523f4d8ea
Jesse Calton <jessecalton@gmail.com>
2020-09-20 20:46:10 -0700

    creates build tasks

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ app.use('/api/todo', require('./routes/todo'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // This is anything that is not the aforementioned backend routes
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  // This is saying "look at the current directory, then look at client, then build, then load index.html"
+}
+
 app.listen(port, () => {
   console.log('Listening on port 5000');
 });
